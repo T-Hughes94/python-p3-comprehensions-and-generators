606ffe66040e1e4bedd9a3d1bdc8129958e68419
Terence Hughes <Terencehughes450@gmail.com>
2023-09-11 18:43:49 -0600

    finished lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return[n for n in num_list if n % 2 == 0]
+return_evens([1, 2, 3, 4, 5, 6])
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + '!' for sentence in sentence_list]
